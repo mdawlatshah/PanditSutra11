0fd4caf276aaae55f0a2b9593d89d9c22a133751
Amir <adawlatshah@gmail.com>
2018-07-12 17:59:32 +0530

    tabs added

---
+++ app/src/main/java/com/example/danial/panditsutra1/PageAdapter.java
@@ -0,0 +1,32 @@
+package com.example.danial.panditsutra1;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class PageAdapter extends FragmentPagerAdapter{
+    private int numOfTabs;
+
+    PageAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ChatFragment();
+            case 1:
+                return new StatusFragment();
+            case 2:
+                return new CallFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {return numOfTabs; }
+
+}
